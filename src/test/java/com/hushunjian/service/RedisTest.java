03daa6753cdf4c15de16b21e7369123db19cd50a
hushunjian <hushunjian950420@163.com>
2018-03-18 20:37:03 +0800

    redistest

---
+++ src/test/java/com/hushunjian/service/RedisTest.java
@@ -0,0 +1,26 @@
+package com.hushunjian.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hushunjian.service.RedisService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+	@Autowired
+	private RedisService redisService;
+	
+	@org.junit.Test
+	public void redisTest(){
+		System.out.println("开始测试");
+		//redisService.set("aa", "AA");
+		redisService.get("aa");
+		//System.out.println("redis存储完毕");
+		System.out.println("redis获取完成"+redisService.get("aa"));
+	}
+	
+}
