519319b60d2b5c8cf532822a8aaa5def6d1ac819
hushunjian <hushunjian950420@163.com>
2020-05-16 15:14:58 +0800

    path

--- src/test/java/com/hushunjian/path/PathTest.java
+++ src/test/java/com/hushunjian/path/PathTest.java
@@ -8,11 +8,14 @@ import org.apache.commons.lang3.StringUtils;
 import com.google.common.collect.Lists;
 
 public class PathTest {
-    private static final List<String> Options = Lists.newArrayList("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    private static final List<Character> OPTIONS = Lists.newArrayList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
+    private static final int OPTIONS_SIZE = OPTIONS.size();
+    
+    
     public static void main(String[] args) {
         System.out.println("候选项个数:");
-        System.out.println(Options.size());
+        System.out.println(OPTIONS.size());
         String outLine = "B";
         char[] chars = outLine.toCharArray();
         for (char option : chars) {
@@ -40,7 +43,7 @@ public class PathTest {
         char[] chars = outLine.toCharArray();
         double result = 0d;
         for (int i = 0; i < chars.length; i++) {
-            result += (Options.indexOf(chars[chars.length - i - 1] + "")) * Math.pow(Options.size(), i);
+            result += (OPTIONS.indexOf(chars[chars.length - i - 1])) * Math.pow(OPTIONS.size(), i);
         }
         return result;
     }
@@ -53,14 +56,32 @@ public class PathTest {
     public static String outLineDoubleToStr(BigDecimal outLine) {
         List<Integer> positions = Lists.newLinkedList();
         while (outLine.compareTo(BigDecimal.ZERO) != 0) {
-            BigDecimal[] nextResult = outLine.divideAndRemainder(BigDecimal.valueOf(Options.size()));
+            BigDecimal[] nextResult = outLine.divideAndRemainder(BigDecimal.valueOf(OPTIONS.size()));
             outLine = nextResult[0];
             positions.add(nextResult[1].intValue());
         }
         StringBuilder str = new StringBuilder();
         for (int i = positions.size() - 1; i >= 0; i--) {
-            str = str.append(Options.get(positions.get(i)));
+            str = str.append(OPTIONS.get(positions.get(i)));
         }
         return StringUtils.leftPad(str.toString(), 3, "0");
+    }
+    
+    public static String next(String path, int step) {
+        char[] chars = path.toCharArray();
+        char[] result = new char[chars.length];
+        for (int i = chars.length - 1; i >= 0; i--) {
+            int remainder = step % OPTIONS_SIZE;
+            int merchant = step / OPTIONS_SIZE;
+            int index = OPTIONS.indexOf(chars[i]);
+            int nextCharIndex = index + remainder;
+            if (nextCharIndex >= OPTIONS_SIZE) {
+                merchant = merchant + nextCharIndex / OPTIONS_SIZE;
+                nextCharIndex = nextCharIndex % OPTIONS_SIZE;
+            }
+            result[i] = OPTIONS.get(nextCharIndex);
+            step = merchant;
+        }
+        return String.valueOf(result);
     }
 }
